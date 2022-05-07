aaf3c2883b69c32b0fe60ace9c58513b52164ff1
PankovGV <dooz2006@mail.ru>
2022-05-07 17:52:13 +0300

    Create constraints

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var yellowTrafficLightSignalView: UIView!
+    @IBOutlet var redTrafficLightSignalView: UIView!
+    @IBOutlet var greenTrafficLightSignalView: UIView!
+    @IBOutlet var showTextButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redTrafficLightSignalView.alpha = 0.3
+        redTrafficLightSignalView.layer.cornerRadius = 75
+        
+        yellowTrafficLightSignalView.alpha = 0.3
+        yellowTrafficLightSignalView.layer.cornerRadius = 75
+        
+        greenTrafficLightSignalView.alpha = 0.3
+        greenTrafficLightSignalView.layer.cornerRadius = 75
+        
+        showTextButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func showTextButtonPressed() {
+       
+        if redTrafficLightSignalView.alpha < 1 &&
+            yellowTrafficLightSignalView.alpha < 1 {
+            redTrafficLightSignalView.alpha = 1
+            greenTrafficLightSignalView.alpha = 0.3
+            showTextButton.setTitle("NEXT", for: .normal)
+        } else if yellowTrafficLightSignalView.alpha < 1 && greenTrafficLightSignalView.alpha < 1 {
+            redTrafficLightSignalView.alpha = 0.3
+            yellowTrafficLightSignalView.alpha = 1
+        } else if greenTrafficLightSignalView.alpha < 1 {
+            yellowTrafficLightSignalView.alpha = 0.3
+            greenTrafficLightSignalView.alpha = 1
+        }
+        
+    }
+    
 }
 
